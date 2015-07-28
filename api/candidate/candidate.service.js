a165130b9d77359b5689c6bfb4a687f82c0f40dc
Michael Kalish <kalishmichael@gmail.com>
2015-07-28 18:49:07 -0400

    Populating the company name

--- api/candidate/candidate.service.js
+++ api/candidate/candidate.service.js
@@ -1,4 +1,7 @@
+'use strict';
+
 var Promise = require('bluebird');
+var _  = require('lodash');
 
 var Candidate = require('../../models/candidate');
 Promise.promisifyAll(Candidate);
@@ -9,11 +12,25 @@ Promise.promisifyAll(Contribution);
 exports.findCandidate = function(candidateId) {
   var contributionsPromise = Contribution.findAsync({candidate: candidateId});
   var candidatePromise = Candidate.findByIdAsync(candidateId);
-
+  var candidateResponse = {};
   return Promise.join(candidatePromise, contributionsPromise, function(candidate, contributions){
-    var candidateResponse = {};
     candidateResponse.candidate = candidate;
-    candidateResponse.contributions = contributions;
+    candidateResponse.individualContributions = _.filter(contributions, function(contribution){
+        return contribution.contributorType === 'Individual';
+    });
+    var corporateContributions = _.filter(contributions, function(contribution){
+      return contribution.contributorType === 'Corporate';
+    });
+
+    return Contribution.populate(corporateContributions, {
+      path: 'contributorName',
+      model: 'Company'
+    });
+
+  })
+  .then(function(populatedCorporateContributions){
+    console.log(populatedCorporateContributions);
+    candidateResponse.companyContributions = populatedCorporateContributions;
     return candidateResponse;
   });
 }
